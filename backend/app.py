81990a4c51640db74cbcd7950e58bc2ae27205ab
mondoboia <giuliarenda94@gmail.com>
2023-01-11 18:46:37 +0100

    sonic results suggestion

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import methods
 from SPARQLWrapper import SPARQLWrapper, JSON
-from sonicclient import SearchClient, IngestClient
+from sonicclient import SearchClient, IngestClient, ControlClient
 
 app = Flask(__name__)
-
-
-@app.route('/feed', methods=['GET'])
-def feed():
-    clips = []
-    # d = methods.read_json('conf_datasets.json')
-    f = methods.read_json('conf_feed.json')
-    for clip in f:
-        clip_info = {}
-        print(clip)
-        clip_info['name'] = f[clip]['name']
-        clip_info['iri'] = f[clip]['iri']
-        clips.append(clip_info)
-    print(clips)
-    return clips
-
 
 def query_sparql(endpoint, subject):
     """
@@ -30,7 +14,7 @@ def query_sparql(endpoint, subject):
     """
 
     value = '<'+subject+'>'
-    print('IRI BASE', value)
+    # print('IRI BASE', value)
     QUERY = """SELECT DISTINCT ?s (SAMPLE(?label) AS ?l)
     WHERE { 
         """+value+""" a ?type . 
@@ -39,11 +23,10 @@ def query_sparql(endpoint, subject):
         } GROUP BY ?s"""
 
     data = get_sparql_results(QUERY, endpoint)
-    print("[SUCCESS] query endpoint:", endpoint)
+    # print("[SUCCESS] query endpoint:", endpoint)
     data = {result["s"]["value"]: result["l"]["value"]
             for result in data["results"]["bindings"] if len(result["l"]["value"]) > 0}
     return data
-
 
 def get_sparql_results(query, endpoint):
     """
@@ -57,6 +40,45 @@ def get_sparql_results(query, endpoint):
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     return results
+
+data = query_sparql('https://projects.dharc.unibo.it/musow/sparql', 'https://w3id.org/musow/1635268063-587743')
+
+
+@app.route('/feed', methods=['GET'])
+def feed():
+    clips = []
+    # d = methods.read_json('conf_datasets.json')
+    f = methods.read_json('conf_feed.json')
+    with IngestClient("127.0.0.1", 1491, "SecretPassword") as ingestcl:
+        print(ingestcl.ping())
+        print(ingestcl.protocol)
+        print(ingestcl.bufsize)
+        for uri, label in data.items():
+            ingestcl.push("polifonia", "entities", uri, label)
+    for clip in f:
+        clip_info = {}
+        print(clip)
+        clip_info['name'] = f[clip]['name']
+        clip_info['iri'] = f[clip]['iri']
+        clips.append(clip_info)
+    # print(clips)
+    return clips
+
+@app.route('/index', methods=['GET'])
+def test():
+    word = request.args.get('data')
+    if len(word) == 0:
+        result = []
+        for label in data.values():
+            result.append(label)
+        return jsonify({'result': result})
+    else:
+        with SearchClient("127.0.0.1", 1491, "SecretPassword") as querycl:
+            print(querycl.ping())
+            # return(querycl.suggest("polifonia", "entities", word))
+            return jsonify({'result': querycl.suggest("polifonia", "entities", word)})
+
+
 
 
 # def sonic_ingest(data, collection="polifonia", bucket="entities"):
@@ -76,8 +98,35 @@ def get_sparql_results(query, endpoint):
 
 data = query_sparql(
     'https://projects.dharc.unibo.it/musow/sparql', 'https://w3id.org/musow/1635268063-587743')
-print(data)
+# print(data)
 # sonic_ingest(data)
+
+# with IngestClient("127.0.0.1", 1491, "SecretPassword") as ingestcl:
+#     print(ingestcl.ping())
+#     print(ingestcl.protocol)
+#     print(ingestcl.bufsize)
+#     for uri, label in data.items():
+#         ingestcl.push("polifonia", "entities", uri, label)
+
+# with SearchClient("127.0.0.1", 1491, "SecretPassword") as querycl:
+#     print(querycl.ping())
+#     print('query', querycl.query("polifonia", "entities", "chopin"))
+#     print('suggest', querycl.suggest("polifonia", "entities", "chop"))
+
+# with IngestClient("127.0.0.1", 1491, "SecretPassword") as ingestcl:
+#     print(ingestcl.ping())
+#     print(ingestcl.protocol)
+#     print(ingestcl.bufsize)
+#     ingestcl.push("wiki", "articles", "article-1", "for the love of god hell")
+#     ingestcl.push("wiki", "articles", "article-2", "for the love of satan heaven")
+#     ingestcl.push("wiki", "articles", "article-3", "for the love of lorde hello")
+#     ingestcl.push("wiki", "articles", "article-4", "for the god of loaf helmet")
+
+# with SearchClient("127.0.0.1", 1491, "SecretPassword") as querycl:
+#     print(querycl.ping())
+#     print(querycl.query("wiki", "articles", "for"))
+#     print(querycl.query("wiki", "articles", "love"))
+#     print(querycl.suggest("wiki", "articles", "hell"))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/Components/Feed/Feed.js
+++ frontend/src/Components/Feed/Feed.js
@@ -4,7 +4,9 @@ class Feed extends React.Component {
     constructor() {
         super();
         this.state = {
-            clips: []
+            clips: [],
+            searchField: '',
+            value_list: []
         }
     }
 
@@ -18,15 +20,42 @@ class Feed extends React.Component {
             )
     }
 
+    onSearchChange = (event) => {
+        // to update state
+        this.setState({ searchField: event.target.value });
+        console.log(event.target.value)
+        let request = '/index?data=' + event.target.value;
+        console.log(request);
+        fetch(request)
+            .then(res => res.json())
+            .then(data => {
+                console.log(Date.now())
+                this.setState({ value_list: data.result });
+            })
+    }
+
     render() {
         return (
             <div>
                 <p>List of clips:</p>
                 <ul>
                     {this.state.clips.map((clip, index) => (
-                        <li key={index}>{clip.name}</li>
+                        <li key={index}>
+                            <p>{clip.name}</p>
+                            <input
+                                type={'search'}
+                                placeholder='change'
+                                onChange={this.onSearchChange}
+                            ></input>
+                        </li>
                     ))}
                 </ul>
+                <div>
+                    {this.state.value_list.map((res, index) => (
+                        <p key={index}>{res}</p>
+                    )
+                    )}
+                </div>
             </div>
         )
     }
