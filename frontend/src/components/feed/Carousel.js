e3cbd23b798a96cbe5991d4900f75ae7e46fbcd3
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-01-23 13:03:19 +0100

    carousel and animations addon

--- frontend/src/components/feed/Carousel.js
+++ frontend/src/components/feed/Carousel.js
@@ -15,16 +15,19 @@ function Carousel() {
           activeItemIndex={activeItemIndex}
           numberOfCards={2}
           gutter={20}
-          slidesToScroll={1}
-          leftChevron={<button>{"<"}</button>}
-          rightChevron={<button>{">"}</button>}
+          slidesToScroll={2}
+          leftChevron={<button className={classes.leftChevron}>{" "}</button>}
+          rightChevron={<button className={classes.rightChevron}>{" "}</button>}
           
           chevronWidth={chevronWidth}
         >
-          <div className={classes.carousel_card} style={{ marginLeft: 60, marginRight: -30}}><h3>Music Knowledge<br/> is now connected!</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
-          <div className={classes.carousel_card} style={{ marginRight: 80}}><h3>Interact with <br/>MUSIC</h3><p>Everyone can play with the infinite music connections of the Polifonia Project.</p></div>
-          <div className={classes.carousel_card} style={{ marginLeft: 80, marginRight: 80}}><h3>Discover the CORPUS</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
-          <div className={classes.carousel_card} style={{ marginLeft: 80, marginRight: 80}}><h3>Play with DATA</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
+          <div className={classes.carousel_card + ' ' +  classes.small_card} id={classes.card0}><h3>Interact with <br/>MUSIC</h3><p><span className={classes.highlight}>Everyone</span> can play with the infinite music connections of the Polifonia Project.</p></div>
+          <div className={classes.carousel_card + ' ' + classes.long_card} id={classes.card1}><h3>Music Knowledge<br/> is now connected!</h3><p>Discovere unexpected connections <br/> with data stories and contents for music experts</p></div>
+          <div className={classes.carousel_card + ' ' +  classes.small_card} ><h3>Discover the CORPUS</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
+          <div className={classes.carousel_card + ' ' +  classes.long_card} ><h3>Play with DATA</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
+          <div className={classes.carousel_card + ' ' +  classes.small_card} ><h3>Play with DATA</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
+          <div className={classes.carousel_card + ' ' +  classes.long_card} ><h3>Play with DATA</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
+          
         </ItemsCarousel>
       </div>
     </div>
