4b93d8f0fe50dcdebcfdf057d18fa1e7fc188fa7
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-01-24 15:19:11 +0100

    background fix

---
+++ frontend/src/components/ui/sectionUi/ResultsTest.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import classes from "./ResultsTest.module.css" 
+
+function ResultsTest() {
+
+    return (
+        <div>
+            Results Test
+        </div>
+    )
+}
+
+export default ResultsTest;
--- frontend/src/components/ui/sectionUi/SectionResults.js
+++ frontend/src/components/ui/sectionUi/SectionResults.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./SectionResults.module.css"
+import ResultsTest from "./ResultsTest";
 
 function SectionResults() {
 
     return (
         <div className={classes.resultContainer}>
+            <ResultsTest></ResultsTest>
         </div>
     )
 }
