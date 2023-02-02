bce63043a99d6d571f926829379ee4230b404372
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-02-02 18:08:05 +0100

    carousel api integrated

--- frontend/src/components/feed/Carousel.js
+++ frontend/src/components/feed/Carousel.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import classes from "./Carousel.module.css";
 import { ThemeContext } from "../../context/ThemeContext";
 import ItemsCarousel from "react-items-carousel";
 
 
-function Carousel() {
+const Carousel = ()  => {
     const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const [content, setContent] = useState([]);
     const chevronWidth = 90;
     const { soundOn, setSoundOn } = useContext(ThemeContext);
+
+    useEffect(() => {
+      fetch("/feed")
+      .then(response => response.json())
+      .then(data => setContent(data.carousel))
+    },[])
 
     const handleClickScroll = (id) => {
       const element = document.getElementById(id);
@@ -32,13 +39,11 @@ function Carousel() {
           
           chevronWidth={chevronWidth}
         >
-          <div className={classes.carousel_card + ' ' +  classes.small_card} id={classes.card0} onClick={() => handleClickScroll('clips_container')}><h3>Interact with <br/>MUSIC</h3><p><span className={classes.highlight}>Everyone</span> can play with the infinite music connections of the Polifonia Project.</p><button></button></div>
-          <div className={classes.carousel_card + ' ' + classes.long_card} id={classes.card1}><h3>Music Knowledge<br/> is now connected!</h3><p>Discover unexpected connections <br/> with data stories and contents for <span className={classes.highlight}>music experts</span></p></div>
-          <div className={classes.carousel_card + ' ' +  classes.small_card} ><h3>Discover the CORPUS</h3><p>Discovere unexpected connections with data stories and contents for music experts</p></div>
-          <div className={classes.carousel_card + ' ' +  classes.long_card} ><h3>Play with DATA</h3><p>Discover unexpected connections with data stories and contents for music experts</p></div>
-          <div className={classes.carousel_card + ' ' +  classes.small_card} ><h3>Play with DATA</h3><p>Discover unexpected connections with data stories and contents for music experts</p></div>
-          <div className={classes.carousel_card + ' ' +  classes.long_card} ><h3>Play with DATA</h3><p>Discover unexpected connections with data stories and contents for music experts</p></div>
-          
+          <div className={classes.carousel_card + ' ' +  classes.small} id={classes.card0} onClick={() => handleClickScroll('clips_container')}><h3>Interact with <br/>MUSIC</h3><p><span className={classes.highlight}>Everyone</span> can play with the infinite music connections of the Polifonia Project.</p><button></button></div>
+          <div className={classes.carousel_card + ' ' + classes.long} id={classes.card1}><h3>Music Knowledge<br/> is now connected!</h3><p>Discover unexpected connections <br/> with data stories and contents for <span className={classes.highlight}>music experts</span></p></div>
+          {Object.values(content).map((card) => (
+            <div className={classes.carousel_card + ' ' +  classes[card.dimension]} ><h3>{card.title}</h3><p>{card.paragraph}</p></div>
+          ))}
         </ItemsCarousel>
       </div>
     </div>
