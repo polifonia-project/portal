ba1128031f6c015ec6699ba5280a7a91000f144c
mondoboia <giuliarenda94@gmail.com>
2023-02-06 17:37:45 +0100

    filters working

--- frontend/src/components/ui/sectionUi/ResultsTest.js
+++ frontend/src/components/ui/sectionUi/ResultsTest.js
@@ -5,22 +5,48 @@ class ResultsTest extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            filtersResults: []
+            filtersResults: [],
+            filtered: [],
+            filterOn: 'False'
         }
     };
 
+    filterItem = (curcat) => {
+        const newItem = (this.state.filtersResults).filter((newVal) => newVal.cat === curcat);
+        this.setState({ filtered: newItem });
+        this.setState({ filterOn: 'True' });
+        console.log(this.filtered);
+    }
+
+    resetFilters = () => {
+        this.setState({ filterOn: 'False' });
+    }
+
     render() {
+        let Data = [];
+        if (this.state.filterOn === 'True') {
+            Data = this.state.filtered;
+        } else if (this.state.filterOn === 'False') {
+            Data = this.state.filtersResults;
+        }
         return (
             <div>
                 <div>
-                    {Object.keys(this.props.filters).map(f => (
-                        <button>{f}</button>
-                    ))}
+                    {Object.keys(this.props.filters).map(f => {
+                        return (
+                            <button onClick={() => this.filterItem(f)}>{f}</button>
+                        )
+                    })}
+                    <button onClick={() => this.resetFilters()}>
+                        Reset
+                    </button>
                 </div>
                 <div>
-                    {this.state.filtersResults.map(res => (
-                        <p>{res.label}</p>
-                    ))}
+                    {Data.map(res => {
+                        return (
+                            <p>{res.label}</p>
+                        )
+                    })}
                 </div>
             </div>
         )
@@ -28,28 +54,32 @@ class ResultsTest extends React.Component {
 
     componentDidMount = () => {
         let results = [];
-        for (const obj of this.props.filters.people) {
-            let endpoint = obj.endpoint;
-            let query = obj.query;
-            query = query.replace('<>', '<' + this.props.el_iri + '>');
-            let url = endpoint + '?query=' + encodeURIComponent(query);
-            try {
-                fetch(url, {
-                    method: 'GET',
-                    headers: { 'Accept': 'application/sparql-results+json' }
-                })
-                    .then((res) => res.json())
-                    .then((data) => {
-                        data.results.bindings.map(res => {
-                            let singleResult = {}
-                            singleResult.uri = res.entity.value;
-                            singleResult.label = res.entityLabel.value;
-                            results.push(singleResult)
-                        })
-                    });
-            }
-            catch (err) {
-                console.log(err)
+
+        for (const cat in this.props.filters) {
+            for (const obj of this.props.filters[cat]) {
+                let endpoint = obj.endpoint;
+                let query = obj.query;
+                query = query.replace('<>', '<' + this.props.el_iri + '>');
+                let url = endpoint + '?query=' + encodeURIComponent(query);
+                try {
+                    fetch(url, {
+                        method: 'GET',
+                        headers: { 'Accept': 'application/sparql-results+json' }
+                    })
+                        .then((res) => res.json())
+                        .then((data) => {
+                            data.results.bindings.map(res => {
+                                let singleResult = {}
+                                singleResult.uri = res.entity.value;
+                                singleResult.label = res.entityLabel.value;
+                                singleResult.cat = cat;
+                                results.push(singleResult)
+                            })
+                        });
+                }
+                catch (err) {
+                    console.log(err)
+                }
             }
         }
         this.setState({ filtersResults: results });
