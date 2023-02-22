aa44ff5be1a1fa899a45a0df6abd640382142493
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-02-22 17:15:02 +0100

    results and filters fixes

--- frontend/src/components/ui/resultsUi/ResultLine.js
+++ frontend/src/components/ui/resultsUi/ResultLine.js
@@ -1,14 +1,35 @@
 import React from "react";
 import classes from "./ResultLine.module.css";
+import { useState } from "react";
+import expandIcon from '../../../assets/svg/expand.svg';
+import expandIconWhite from '../../../assets/svg/expandWhite.svg';
 
 function ResultLine(props) {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+     setIsHover(true);
+  };
+  const handleMouseLeave = () => {
+     setIsHover(false);
+  };
+
+
   return (
     <div className={classes.resultLine}>
       <div className={classes.resultNum}>{props.number}</div>
       <div className={classes.resultCat}><span className={classes.categoryResult}>{props.cat}</span></div>
       <div className={classes.resultLabel}><b>{props.label}</b></div>
       <div className={classes.resultRel}>Wolfgang Amadeus Mozart&nbsp;&nbsp;————&nbsp;&nbsp; {props.rel} &nbsp;&nbsp;————&nbsp;&nbsp; {props.label}</div>
-      <div className={classes.resultActions}><button></button></div>
+      <div className={classes.resultActions}>
+        <button 
+          style={{backgroundColor: isHover ? props.color : 'transparent',
+                  border: isHover ? 'transparent' : '0.5px solid black',
+                  backgroundImage: isHover ? `url(${expandIconWhite})` : `url(${expandIcon})` }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        ></button>
+      </div>
     </div>
   )
 }
--- frontend/src/components/ui/resultsUi/Results.js
+++ frontend/src/components/ui/resultsUi/Results.js
@@ -10,59 +10,107 @@ class ResultsTest extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            filtersResults: [],
-            filtered: [],
+            totalResults: [],
+            filteredResults: [],
+            activeFilters: [],
             filterOn: false,
             relationOn: false,
             relations: []
         }
     };
 
-    filterItem = (curcat) => {
-        const newItem = (this.state.filtersResults).filter((newVal) => newVal.cat === curcat);
-        this.setState({ filtered: newItem });
-        this.setState({ filterOn: true });
-        console.log(this.filtered);
+    addFilter = (curcat) => {
+        // register added or removed filters to list
+        let currentFilters = this.state.activeFilters;
+        if (this.state.activeFilters.indexOf(curcat) > -1) {
+            const newFilters = currentFilters.filter((item) => item !== curcat);
+            currentFilters = newFilters;
+        } else {
+            currentFilters.push(curcat)
+        }
+        this.setState({ activeFilters: currentFilters });
+
+        // apply filters
+        this.applyFilter(currentFilters);
+
+       // set filter on alert
+       if (currentFilters.length === 0) {
+        this.setState({ filterOn: false });
+       } else {
+        this.setState({ filterOn: true});
+       }
+
+    }
+
+    addRelation = (curcat) => {
+        // register added or removed filters to list
+        let currentFilters = this.state.activeFilters;
+        if (this.state.activeFilters.indexOf(curcat) > -1) {
+            const newFilters = currentFilters.filter((item) => item !== curcat);
+            currentFilters = newFilters;
+        } else {
+            currentFilters.push(curcat)
+        }
+        this.setState({ activeFilters: currentFilters });
+
+        // apply filters
+        this.applyFilter(currentFilters);
+
+       // set filter on alert
+       if (currentFilters.length === 0) {
+        this.setState({ relationOn: false });
+       } else {
+        this.setState({ relationOn: true});
+       }
+
+    }
+
+    applyFilter = (currentFilters) => {
+        // apply filters thare are inside list
+       const newItem = (this.state.totalResults).filter((newVal) => currentFilters.indexOf(newVal.cat) > -1 || currentFilters.indexOf(newVal.rel) > -1);
+       this.setState({ filteredResults: newItem });
     }
 
     resetFilters = () => {
         this.setState({ filterOn: false });
+        this.setState({ relationOn: false });
+        this.setState({ activeFilters: [] });
     }
 
     render() {
         let Data = [];
-        if (this.state.filterOn === true) {
-            Data = this.state.filtered;
-        } else if (this.state.filterOn === false) {
-            Data = this.state.filtersResults;
+        if (this.state.activeFilters.length > 0) {
+            Data = this.state.filteredResults;
+        } else if (this.state.activeFilters.length === 0) {
+            Data = this.state.totalResults;
         }
         return (
             <div>
                 <ResultsHeader>
                 <FiltersContainer>
-                    <FilterButton isDisabled={this.state.filterOn} resetClass = 'resetButton' buttonClick={() => this.resetFilters()}>
+                    <FilterButton isDisabled={this.state.filterOn || this.state.relationOn} resetClass = 'resetButton' buttonClick={() => this.resetFilters()}>
                       Reset <span className="resetIcon">⟲</span>
                     </FilterButton> <br/>
                     <Filters filtersType="Filters" color= {this.props.color} selectedOn={this.state.filterOn}>
                         {Object.keys(this.props.filters).map(f => {
                             return (
-                                <FilterButton isDisabled={true} buttonClick={() => this.filterItem(f)} selectedOn={this.state.filterOn}>{f}</FilterButton>
+                                <FilterButton isDisabled={true} buttonClick={() => this.addFilter(f)} selectedOn={this.state.filterOn}>{f}</FilterButton>
                             )
                         })}
                     </Filters>
                     <Filters filtersType="Relations" color= {this.props.color} selectedOn={this.state.relationOn}>
                         {this.state.relations.map(rel => {
                             return (
-                                <FilterButton isDisabled={true} selectedOn={this.state.relationOn}>{rel}</FilterButton>
+                                <FilterButton isDisabled={true} buttonClick={() => this.addRelation(rel)}  selectedOn={this.state.relationOn}>{rel}</FilterButton>
                             )
                         })}
                     </Filters>
                 </FiltersContainer>
                 </ResultsHeader>
-                <div style= {{height: '390px', overflow: 'scroll' }}>
+                <div style= {{height: '400px', overflow: 'scroll' }}>
                     {Data.map((res, index) => {
                         return (
-                            <ResultLine label={res.label} rel={res.rel} cat={res.cat} number={index + 1}></ResultLine>
+                            <ResultLine label={res.label} rel={res.rel} cat={res.cat} number={index + 1} color= {this.props.color}></ResultLine>
                         )
                     })}
                 </div>
@@ -72,7 +120,6 @@ class ResultsTest extends React.Component {
 
     componentDidMount = () => {
         let results = [];
-
         let relations = [];
 
         // get dataset
@@ -119,9 +166,9 @@ class ResultsTest extends React.Component {
                     }
                 }
             })
-        console.log(results)
-        this.setState({ filtersResults: results });
-        this.setState({ relations: relations })
+        console.log(results);
+        this.setState({ totalResults: results });
+        this.setState({ relations: relations });
     };
 }
 
