348d9cb7c429a3be91cbfaa3418140b4e109b284
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-03-13 17:36:13 +0100

    barchart caption and hovering

--- frontend/src/components/ui/resultsUi/SourcesBarchart.js
+++ frontend/src/components/ui/resultsUi/SourcesBarchart.js
@@ -1,28 +1,39 @@
 import React from "react";
 import classes from "./SourcesBarchart.module.css";
+import { useState } from "react";
 
 function SourcesBarchart() {
 
   const a = { 
-    'pilot dataset': 10,
-    'pilot data': 20,
     'musow': 50,
-    'wikidata': 70,
-    'choco': 50,
-    'sound and vision': 20,
-    'polifonia': 10,
+    'wikidata': 30,
+    'choco': 70,
+    'sound and vision': 90,
+    'polifonia': 30,
   }
+
+  const [isShown, setIsShown] = useState(false);
+  const [caption, setCaption] = useState('');
+  const [textValue, setValue] = useState('');
 
   return (
     <div className={classes.barchartContainer}>
       <div className={classes.barchartBox}>
-        {Object.values(a).map(value => (
-            <span className={classes.barLine}>
+        {Object.entries(a).map(([key,value])=> (
+            <span className={classes.barLine} 
+            onMouseEnter={() => {
+              setIsShown(true);
+              setCaption(key);
+              setValue(value);
+            }}
+              onMouseLeave={() => setIsShown(false)}
+            >
             <span className={classes.barLineRight} style={{height: value + '%'}}/>
             <span className={classes.barLineLeft} style={{height: value + '%'}}/>
             </span>
         ))}
       </div>
+      <div className={classes.barchartText} style={{display: isShown ? 'block' : 'none'}}><span className={classes.datasource}>Data source - </span> {caption} ({textValue}%)</div>
     </div>
   );
 }
