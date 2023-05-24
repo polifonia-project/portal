0f2afb234ecdcb4dc729e413810630b2b16f442d
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-05-24 18:29:40 +0200

    sample card added

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -10,6 +10,11 @@ import VisualBlock from "./VisualBlock";
 import LinkBlock from "./LinkBlock";
 import WarningBlock from "./WarningBlock";
 
+import TextBlockFake from "./TextBlockFake";
+import LinkBlockFake from "./LinkBlockFake";
+import RelationBlockFake from "./RelationBlockFake";
+import VisualBlockFake from "./VisualBlockFake";
+
 function Card(props) {
 
   const { cardOpen, setCardOpen } = useContext(CardContext);
@@ -18,6 +23,7 @@ function Card(props) {
 
   const [colorBackground, setColorBackground] = useState('#e2e2e2')
   const [colorIsDark, setColorIsDark] = useState(false)
+  const [isManchester,  setManchester] = useState(false)
   const [currentBlock, setCurrentBlock] = useState({})
   const [fromSectionClip, setFromSectionClip] = useState(false)
 
@@ -39,6 +45,10 @@ function Card(props) {
       } else {
         setColorIsDark(false);
         setDarkHeader();
+      }
+
+      if (cardContent.title === 'Manchester Digital Music Archive') {
+        setManchester(true);
       }
 
     } 
@@ -88,29 +98,37 @@ function Card(props) {
           <button className={classes.exitButton} onClick={()=> closeCard()} style={{color: colorIsDark ? 'white' : 'black', borderColor: colorIsDark ? 'white' : '#474747'}}>Back ⇢</button>
         </div>
       </div>
+      {isManchester ?
       <div className={classes.contentBlock}>
-
-        {Object.values(currentBlock).map((block, i) => {
-          if (block.type === 'text') {
-            return <TextBlock key={'textblock-' + i} width={block.size} title={block.title}></TextBlock>} 
-
-          else if (block.type === 'relation')
-          { return <RelationBlock key={'relationblock-' + i} width={block.size} title={block.title}></RelationBlock>}
-
-          else if (block.type === 'link')
-          { return <LinkBlock key={'linkblock-' + i} width={block.size} title={block.title} links={block.content}></LinkBlock>}
-
-          else if (block.type === 'visual')
-          { return <VisualBlock key={'visualblock-' + i} width={block.size} title={block.title}></VisualBlock>}
-
-          else if (block.type === 'none')
-          { return <WarningBlock key={'warningblock-' + i} width={'large'}></WarningBlock>}
-          return null
-        })}
-
-        
-
+      <TextBlockFake key={'textblock-fake01'} width={"small"} title={"Description"}></TextBlockFake>
+      <RelationBlockFake key={'relationblock-fake01'} width={"small"} title={"Collected resources"}></RelationBlockFake>
+      <RelationBlockFake key={'relationblock-fake01'} width={"small"} title={"Bands"}></RelationBlockFake>
+      <LinkBlockFake key={'linkblock-fake01'} width={"small"} title={"Useful Links"} links={"one"}></LinkBlockFake>
+      <VisualBlockFake key={'visualblock-fake01'} width={"medium"} title={"Photograph"}></VisualBlockFake>
+      <TextBlockFake key={'textblock-fake01'} width={"small"} title={""}></TextBlockFake>
+      
       </div>
+       : 
+      <div className={classes.contentBlock}>
+      {Object.values(currentBlock).map((block, i) => {
+        if (block.type === 'text') {
+        return <TextBlock key={'textblock-' + i} width={block.size} title={block.title}></TextBlock>} 
+
+        else if (block.type === 'relation')
+        { return <RelationBlock key={'relationblock-' + i} width={block.size} title={block.title}></RelationBlock>}
+
+        else if (block.type === 'link')
+        { return <LinkBlock key={'linkblock-' + i} width={block.size} title={block.title} links={block.content}></LinkBlock>}
+
+        else if (block.type === 'visual')
+        { return <VisualBlock key={'visualblock-' + i} width={block.size} title={block.title}></VisualBlock>}
+
+        else if (block.type === 'none')
+        { return <WarningBlock key={'warningblock-' + i} width={'large'}></WarningBlock>}
+        return null
+      })}
+      </div>
+      }
     </div>
   );
 }
