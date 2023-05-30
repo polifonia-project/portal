f69762dabbe9d9cd0de5a032f693f14ecb0d354e
mondoboia <giuliarenda94@gmail.com>
2023-05-30 16:53:15 +0200

    store triples in nt file

--- backend/reconciliation.py
+++ backend/reconciliation.py
@@ -4,10 +4,14 @@ import re
 from flask import request, Response
 import requests
 from SPARQLWrapper import SPARQLWrapper, POST, JSON, DIGEST
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import SDO, RDFS, OWL
 
 MYLINKSET = 'http://localhost:9999/bigdata/sparql'
 UPDATEMYLINKSET = 'http://localhost:9999/blazegraph/namespace/kb/sparql/update'
 LILNKSETGRAPH = 'http://w3id.org/polifonia/linkset/'
+
+WHITE_LIST = ['wikidata', 'dbpedia', 'viaf', 'discogs']
 
 
 def __run_query_string(active, query_string,
@@ -61,23 +65,27 @@ def __contact_tp(data, is_post, content_type):
         #                        text=req.text)
 
 
-def query_same_as(uri_list):  # altro parametro tipo lista con sparqlenpoint
+# altro parametro tipo lista con sparqlenpoint
+def query_same_as_internal(uri_list):
     values_to_search = ' '.join(uri_list)
-    find_query = '''
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    SELECT DISTINCT ?origin_uri ?same_uri
-    WHERE {
-        VALUES ?origin_uri {'''+values_to_search+'''} .
-        ?same_uri owl:sameAs|skos:exactMatch|^owl:sameAs|^skos:exactMatch ?origin_uri .
-    }
-    '''
-    return find_query
+    if len(values_to_search) < 1500:
+        find_query = '''
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        SELECT DISTINCT ?origin_uri ?same_uri
+        WHERE {
+            VALUES ?origin_uri {'''+values_to_search+'''} .
+            ?same_uri owl:sameAs|skos:exactMatch|^owl:sameAs|^skos:exactMatch ?origin_uri .
+        }
+        '''
+        return find_query
+    else:
+        print('[NEED ACTION] values_to_search too long.')
 
 
 def find_matches(uri_list, endpoint):
     sparql = SPARQLWrapper(endpoint)
-    query = query_same_as(uri_list)
+    query = query_same_as_internal(uri_list)
     sparql.setQuery(query)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -85,21 +93,6 @@ def find_matches(uri_list, endpoint):
                for result in results['results']['bindings'] if len(result['same_uri']['value']) > 0}
     # {origin_uri: same_uri}
     return results
-
-
-def linkset_population(datasets, dataset, uri_list):
-    uris_to_search = []
-    for uri in uri_list:
-        uris_to_search.append('<' + uri + '>')
-        # find matches in all other datasets
-        for d in datasets:
-            if d != dataset:  # da togliere
-                sparql_endpoint = datasets[d]['sparql_endpoint']
-                same_uris_dict = find_matches(uris_to_search, sparql_endpoint)
-                for origin_uri, same_uri in same_uris_dict.items():
-                    linkset_update(datasets[dataset]['sparql_endpoint'], datasets[dataset]['name'],
-                                   origin_uri, same_uri, datasets[d]['sparql_endpoint'], datasets[d]['name'])
-        print('[SUCCESS] linkest populated')
 
 
 def linkset_update(dataset_1, dataset_1_label, uri_1, same_uri, dataset_2, dataset_2_label):
@@ -145,3 +138,45 @@ def clear_linkset():
     sparql.setQuery(delete_query)
     sparql.query()
     print('[DELETE] linkeset emptied')
+
+
+def parse_ntriple_linkest(file):
+    g = Graph()
+    g.parse(file, format='nt')
+    return g
+
+
+def write_ntriple_linkset(g, file):
+    g.serialize(destination=file, format='nt')
+
+
+def add_triples_to_linkset(g, dataset_1, dataset_1_label, uri_1, same_uri, dataset_2, dataset_2_label):
+    # uri_1
+    g.add((URIRef(uri_1), SDO.location, URIRef(dataset_1)))
+    g.add((URIRef(uri_1), OWL.sameAs, URIRef(same_uri)))
+    g.add((URIRef(dataset_1), RDFS.label, Literal(dataset_1_label)))
+    # same_uri
+    g.add((URIRef(same_uri), SDO.location, URIRef(dataset_2)))
+    g.add((URIRef(same_uri), OWL.sameAs, URIRef(uri_1)))
+    g.add((URIRef(dataset_2), RDFS.label, Literal(dataset_2_label)))
+
+
+def linkset_population(datasets, dataset, uri_list):
+    linkset_graph = parse_ntriple_linkest('linkset.nt')
+    uris_to_search = []
+    for uri in uri_list:
+        if any((match := substring) in uri for substring in WHITE_LIST):
+            print(match, 'try something else')
+        else:
+            uris_to_search.append('<' + uri + '>')
+    print('TO SEARCH', uris_to_search)
+    # find matches in all datasets
+    if len(uris_to_search) > 0:
+        for d in datasets:
+            sparql_endpoint = datasets[d]['sparql_endpoint']
+            same_uris_dict = find_matches(uris_to_search, sparql_endpoint)
+            for origin_uri, same_uri in same_uris_dict.items():
+                add_triples_to_linkset(linkset_graph, datasets[dataset]['sparql_endpoint'], datasets[dataset]['name'],
+                                       origin_uri, same_uri, datasets[d]['sparql_endpoint'], datasets[d]['name'])
+        write_ntriple_linkset(linkset_graph, 'linkset.nt')
+        print('[SUCCESS] linkest populated')
