72cdfdf3aca2ade1513f684d689b073f2d616b14
mondoboia <giuliarenda94@gmail.com>
2023-07-11 22:01:50 +0200

    moved cleaning functions

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # access all conf files: datasets (d), categories (cat), feed info (f) and carouse (car)
 d, cat, f, car, cards = methods.access_conf_info('conf_general.json')
 i.ingest_data(d, cat)
-rec.clear_linkset(False, rec.LINKSET_FILE)
+endpoint.clear_linkset(False, rec.LINKSET_FILE)
 
 
 @app.route('/conf_info', methods=['GET'])
@@ -20,7 +20,7 @@ def feed():
     f_list = []
     for clip in f.values():
         f_list.append(clip)
-    return jsonify({'datasets': d, 'categories': cat, 'carousel': car, 'cards': cards,'clips': f_list})
+    return jsonify({'datasets': d, 'categories': cat, 'carousel': car, 'cards': cards, 'clips': f_list})
 
 
 @app.route('/index', methods=['GET'])
