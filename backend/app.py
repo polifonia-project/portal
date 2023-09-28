52dfa475f4972ccfb03c7b4345b07ed2343ad14a
giuliarenda <giuliarenda94@gmail.com>
2023-09-28 09:28:04 +0200

    functions at flask run

--- backend/app.py
+++ backend/app.py
@@ -14,23 +14,6 @@ app = Flask(__name__)
 
 # access all conf files: datasets (d), categories (cat), feed info (f) and carouse (car)
 d, cat, f, car, cards = methods.access_conf_info('conf_general.json')
-# i.ingest_data(d, cat)
-
-clear_linkset_result = timeit.timeit(
-    stmt='endpoint.clear_linkset(False, endpoint.LINKSET_DIRECTORY, endpoint.ENTITIES_DIRECTORY)', globals=globals(), number=1)
-print(f"Execution time for clear_linkset is {clear_linkset_result} seconds")
-
-fill_entities_files_result = timeit.timeit(
-    stmt='methods.fill_entities_files("OFF", cat, d, endpoint.ENTITIES_DIRECTORY)', globals=globals(), number=1)
-print(
-    f"Execution time for fill_entities_files is {fill_entities_files_result} seconds")
-
-linkset_endpoint_update_result = timeit.timeit(
-    stmt='endpoint.linkset_endpoint_update(endpoint.ENTITIES_DIRECTORY, d, endpoint.LINKSET_DIRECTORY)', globals=globals(), number=1)
-print(
-    f"Execution time for linkset_endpoint_update is {linkset_endpoint_update_result} seconds")
-
-# rec.graphs_reconciliation(endpoint.ENTITIES_DIRECTORY, endpoint.UPDATEMYLINKSET)
 
 
 @app.route('/conf_info', methods=['GET'])
@@ -46,7 +29,7 @@ def index():
     cat_id = request.args.get('cat_id')
     word = request.args.get('data')
     if len(word) > 0:
-        suggestions = i.suggested_results(d, cat, cat_id, word.lower())
+        suggestions = i.suggested_results(d, cat, cat_id, word.lower(), endpoint.ENTITIES_DIRECTORY)
         return jsonify(suggestions)
 
 
@@ -74,6 +57,30 @@ def reconciliation(active=None):
         elif "application/sparql-query" in content_type:
             return endpoint.__contact_tp(cur_data, True, content_type)
 
+# @app.before_first_request
+# def setup_data(clean=False, find_entities='OFF'):
+#     clear_linkset_result = timeit.timeit(
+#         stmt='endpoint.clear_linkset(clean, endpoint.LINKSET_DIRECTORY, endpoint.ENTITIES_DIRECTORY)', globals=globals(), number=1)
+#     print(f"Execution time for clear_linkset is {clear_linkset_result} seconds")
+
+#     fill_entities_files_result = timeit.timeit(
+#         stmt='methods.fill_entities_files(find_entities, cat, d, endpoint.ENTITIES_DIRECTORY)', globals=globals(), number=1)
+#     print(
+#         f"Execution time for fill_entities_files is {fill_entities_files_result} seconds")
+
+#     ingest_chosen_index_result = timeit.timeit(
+#         stmt='i.ingest_chosen_index(cat, endpoint.ENTITIES_DIRECTORY, "GENERIC")', globals=globals(), number=1)
+#     print(f"Execution time for ingest_chosen_index is {ingest_chosen_index_result} seconds")
+
+    # linkset_endpoint_update_result = timeit.timeit(
+    #     stmt='endpoint.linkset_endpoint_update(endpoint.ENTITIES_DIRECTORY, d, endpoint.LINKSET_DIRECTORY)', globals=globals(), number=1)
+    # print(
+    #     f"Execution time for linkset_endpoint_update is {linkset_endpoint_update_result} seconds")
+
+    # graphs_reconciliation_result = timeit.timeit(
+    #     stmt='rec.graphs_reconciliation(endpoint.ENTITIES_DIRECTORY, endpoint.UPDATEMYLINKSET)', globals=globals(), number=1)
+    # print(
+    #     f"Execution time for graphs_reconciliation is {graphs_reconciliation_result} seconds")
 
 if __name__ == '__main__':
     app.run(debug=True)
