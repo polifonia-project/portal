438bb211bfa1b603ad13e5719590ccfadc8e1328
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-10-27 11:38:03 +0200

    filters redesign

--- frontend/src/components/ui/resultsUi/Filters.js
+++ frontend/src/components/ui/resultsUi/Filters.js
@@ -5,7 +5,7 @@ function Filters(props) {
 
   const [toggle, setToggle] = useState(false)
 
-  return ([ 
+  return ([
     <div key={'filters-name-' + props.cat} className={classes.filtersType + ' ' + classes[toggle ? 'typeOpen' : 'typeClosed']} onClick={() => setToggle(!toggle)}>
       <span style={{ position: 'relative' }} className={classes.typeName}>{props.filtersType}<span className={classes.slider}></span></span>
       {toggle ? <span className={classes.closeIcon}> −</span> : props.selectedOn ? <span><span className={classes.selectedDot} style={{ color: props.color }}>•</span><span className={classes.openIcon}>  +</span></span> : <span className={classes.openIcon}> +</span>}
