e4e02d5cd138d53f810b210297f2d4e788f323ee
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-10-27 11:41:32 +0200

    filters redesigned and new grouping fix #12

--- frontend/src/components/ui/resultsUi/FiltersContainer.js
+++ frontend/src/components/ui/resultsUi/FiltersContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./FiltersContainer.module.css";
 
 function FiltersContainer(props) {
+  
+   const [filtersOpen, setFiltersOpen] = useState(false);
+    
+   const handleClickOpen = (e) => {setFiltersOpen(true)}
+   const handleClickClose = (e) => {setFiltersOpen(false)}
+
 
     return (
     <div className={classes.filtersContainer}>
-      {props.children}
+      <div className={classes.filtersButtonContainer}>
+        {filtersOpen ? 
+          [<span onClick={() => handleClickClose()} className={classes.filterButton}>Filters <span className={classes.slider}></span></span>,<span className={classes.closeIcon}>−</span>]
+        : [<span onClick={() => handleClickOpen()} className={classes.filterButton}>Filters <span className={classes.slider}></span></span>,<span className={classes.openIcon}>+</span>] }
+      </div>
+      <div className={classes.filtersTypesContainer}>
+      {filtersOpen ? props.children : null }
+      </div>
     </div>
     )
 }
