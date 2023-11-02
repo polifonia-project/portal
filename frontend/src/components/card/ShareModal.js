dd8fbd6d8cf5b9442ba501863a62d483bbe21353
marcograsso <56439271+marcograsso@users.noreply.github.com>
2023-11-02 15:48:25 +0100

    updates close button and close options in share modal #14

--- frontend/src/components/card/ShareModal.js
+++ frontend/src/components/card/ShareModal.js
@@ -4,16 +4,24 @@ import { ShareSocial } from 'react-share-social'
 
 function ShareModal(props) {
 
+  const handleParentClick = event => {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+      props.changeDisplay(false)
+    }
+  };
+
   const style = {
     root: {
       backgroundColor: '#f4edec',
       borderRadius: '8px',
       border: 0,
       boxShadow: 'rgb(124 123 123 / 13%) -1px 5px 30px 1px',
-      color: 'white',
+      color: 'grey',
       width: '80vw',
-      marginBottom: '20px',
-      padding: '12px 30px 30px 30px'
+      marginBottom: '30px',
+      padding: '12px 20px 30px 30px'
     },
     copyContainer: {
       backgroundColor: 'rgb(217 214 214)',
@@ -24,15 +32,20 @@ function ShareModal(props) {
     }
   };
 
+
+
   return (
-    <div className={classes.modalContainer} style={{ visibility: props.display ? "visible" : "hidden", opacity: props.display ? "1" : "0", }}>
-      <button className={classes.closeButton} onClick={() => props.changeDisplay(false)}>Close</button>
+    <div className={classes.modalContainer} style={{ visibility: props.display ? "visible" : "hidden", opacity: props.display ? "1" : "0", }}
+       onClick={handleParentClick}>
+      <span>
+      <button className={classes.closeButton} onClick={() => props.changeDisplay(false)}>╳</button>
       <ShareSocial
         url={props.url}
         socialTypes={['facebook', 'twitter', 'whatsapp', 'linkedin', 'telegram', 'reddit']}
         onSocialButtonClicked={data => console.log(data)}
         style={style}
       />
+      </span>
     </div>
   );
 }
